386f6360357f1f22b1a0b54ab7ebdaecdc269cf3
Mooneer Salem <mooneer@gmail.com>
2014-04-15 23:12:44 -0700

    Added basic form processing.

--- volunteer-management-plugin.php
+++ volunteer-management-plugin.php
@@ -13,11 +13,9 @@ License: unknown
 // Usage (in pages): [mmf_form form_url=/form.php]
 // Parameters:
 //     form_url: URL for form to be posted to (optional).
-// NOTE: does not currently process data.
 function mmf_show_form($args, $content = null) {
     $form_url = $args['form_url'];
-
-    return <<<END
+    $result = <<<END
 <form method="POST" action="$form_url">
 First name: <input type="text" name="mmf_first_name" /><br/>
 Last name: <input type="text" name="mmf_last_name" /><br/>
@@ -25,6 +23,24 @@ Email: <input type="email" name="mmf_email" /><br/>
 <input type="submit" />
 </form>
 END;
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        if (empty($_POST['mmf_first_name']) || empty($_POST['mmf_last_name']) || empty($_POST['mmf_email']))
+        {
+            $x = <<<END
+<p><b>All fields are required.</b></p>
+END;
+            $result = $x . $result;
+        }
+        else
+        {
+            // TODO: send email. Or use MailChimp. Or something.
+            $result = "Welcome, " . htmlspecialchars($_POST['mmf_first_name']) . " " . htmlspecialchars($_POST['mmf_last_name']) . ", to the list.";
+        }
+    }
+
+    return $result;
 }
 
 add_shortcode('mmf_form', 'mmf_show_form');
